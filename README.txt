2d994cbcaaf74cb76b02677c012e8073fac58265
Sergio Pascual <sergiopr@fis.ucm.es>
2014-03-21 12:43:49 +0100

    Create standard python package layout

---
+++ README.txt
@@ -0,0 +1,7 @@
+This PySQM...
+
+PySQM is distributed under GNU GPL, either version 3 of the License, 
+or (at your option) any later version. See the file LICENSE.txt for details.
+
+This software has been developed by Miguel Nievas <miguelnievas@ucm.es>
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysqm',
+      version='0.0.1dev',
+      author='Miguel Nievas',
+      author_email='miguelnievas@ucm.es',
+      url='http://guaix.fis.ucm.es/hg/pysqm/',
+      license='GPLv3',
+      description='SQM reading and plotting software',
+      packages=['pysqm'],
+      classifiers=[
+                   "Programming Language :: C",
+                   "Programming Language :: Cython",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: Implementation :: CPython",
+                   'Development Status :: 3 - Alpha',
+                   "Environment :: Other Environment",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: GNU General Public License (GPL)",
+                   "Operating System :: OS Independent",
+                   "Topic :: Scientific/Engineering :: Astronomy",
+                   ],
+      long_description=open('README.txt').read()
+      )
